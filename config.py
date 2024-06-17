8333edc3099655bd2fb9e410e152a3e805293e73
TIGER <141488144+tiger7815@users.noreply.github.com>
2024-06-18 01:42:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("6871505238:AAF0DiNwF2n5VmmLE77xwdb2zrZrgECWe24")
 WATERMARK = str(os.environ.get("WATERMARK", "...watermark..."))
 FONT_SIZE = int(os.environ.get("FONT_SIZE", 15))
 FONT_NAME = "Space_Age.ttf"  # check tools/fonts
