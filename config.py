4181acb783b77c5eac920d2f26bf5e50e0304c0a
wddwwdw <hangerskype@gmail.com>
2026-05-12 18:02:05 +0530

    Update and rename config.py to runtime.txt

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-WATERMARK = str(os.environ.get("WATERMARK", "...watermark..."))
-FONT_SIZE = int(os.environ.get("FONT_SIZE",9))
-FONT_NAME = "Space_Age.ttf"  # check tools/fonts
-TRANSPARENCY = float(os.environ.get("TRANSPARENCY", 0.75))
-QUALITY = int(os.environ.get("QUALITY", 100))
